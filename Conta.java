d145f7cb22bc692e24da7909621c83bfd7eeded1
LeoValleWeb <leomartinsvalle@gmail.com>
2023-02-28 14:44:48 +0000

    ContaBanco

---
+++ Conta.java
@@ -0,0 +1,6 @@
+public class Conta {
+    int numero;
+    String titulo;
+    double saldo;
+
+}
---
+++ programa.java
@@ -0,0 +1,6 @@
+public class programa {
+    public static void main(String[] args) {
+        Conta minhaConta;
+        minhaConta = new Conta();
+    }
+}
